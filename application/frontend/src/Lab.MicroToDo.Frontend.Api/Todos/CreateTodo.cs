2f3484c03127f15c248af25d8466ef3719fa5678
Tobias Richling <richlint@apetito.com>
2023-11-10 12:18:41 +0100

    load base url from config and allow flagging of backend call

--- application/frontend/src/Lab.MicroToDo.Frontend.Api/Todos/CreateTodo.cs
+++ application/frontend/src/Lab.MicroToDo.Frontend.Api/Todos/CreateTodo.cs
@@ -6,8 +6,18 @@ namespace Lab.MicroToDo.Frontend.Api.Todos;
 public static class CreateTodo
 {
 
-    public static async Task<IResult> Handle(TodoViewModel todoViewModel, [FromServices] IHttpClientFactory httpClientFactory)
+    public static async Task<IResult> Handle(
+        TodoViewModel todoViewModel, 
+        [FromServices] IHttpClientFactory httpClientFactory,
+        [FromServices] IConfiguration configuration)
     {
+        var useTodosService = bool.TryParse(configuration["FeatureFlags:UseTodosService"], out var useTodosServiceResult) ? useTodosServiceResult : false;
+
+        if (!useTodosService)
+        {
+            return Results.Ok(todoViewModel);
+        }
+
         todoViewModel.Id = Guid.NewGuid();
         todoViewModel.IsCompleted = false;
 
--- application/frontend/src/Lab.MicroToDo.Frontend.Api/Todos/GetAllTodos.cs
+++ application/frontend/src/Lab.MicroToDo.Frontend.Api/Todos/GetAllTodos.cs
@@ -6,8 +6,46 @@ namespace Lab.MicroToDo.Frontend.Api.Todos;
 public static class GetAllTodos
 {
 
-    public static async Task<IResult> Handle([FromServices] IHttpClientFactory httpClientFactory)
+    public static async Task<IResult> Handle(
+        [FromServices] IConfiguration configuration, 
+        [FromServices] IHttpClientFactory httpClientFactory)
     {
+        var useTodosService = bool.TryParse(configuration["FeatureFlags:UseTodosService"], out var useTodosServiceResult) ? useTodosServiceResult : false;
+
+        if (!useTodosService)
+        {
+            // return fake data
+            var fakeTodos = new TodoListViewModel
+            {
+                Todos = new List<TodoViewModel>
+                {
+                    new TodoViewModel
+                    {
+                        Id = Guid.NewGuid(),
+                        Title = "Todo 1",
+                        Description = "Description 1",
+                        IsCompleted = false
+                    },
+                    new TodoViewModel
+                    {
+                        Id = Guid.NewGuid(),
+                        Title = "Todo 2",
+                        Description = "Description 2",
+                        IsCompleted = false
+                    },
+                    new TodoViewModel
+                    {
+                        Id = Guid.NewGuid(),
+                        Title = "Todo 3",
+                        Description = "Description 3",
+                        IsCompleted = false
+                    }
+                }
+            };
+
+            return Results.Ok(fakeTodos);
+        }
+
         var httpClient = httpClientFactory.CreateClient("todos-api");
         var response = await httpClient.GetAsync("/todos");
         if (!response.IsSuccessStatusCode)
