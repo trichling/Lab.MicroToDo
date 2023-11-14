23ae9f616f58f6bdc7e6aeca64f0f5e0fb991953
Tobias Richling <richlint@apetito.com>
2023-11-14 22:42:42 +0100

    some minor tweaks

--- infrastructure/kubernetes/ingress/install-ingresscontroller.ps1
+++ infrastructure/kubernetes/ingress/install-ingresscontroller.ps1
@@ -1,6 +1,6 @@
 param (
     [Parameter(Mandatory)] [string] $Environment,
-    [Parameter] [string] $Version
+    [Parameter(Mandatory)] [string] $Version
 )
 
 $prevPwd = $PWD; Set-Location -ErrorAction Stop -LiteralPath $PSScriptRoot
@@ -15,15 +15,17 @@ $dnsLabel = "$application-$Environment-$Version"
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 
-kubectl create namespace ingress-nginx
-helm install nginx-ingress ingress-nginx/ingress-nginx `
+kubectl create namespace ingress-nginx --dry-run=client -o yaml | kubectl apply -f -
+helm upgrade nginx-ingress ingress-nginx/ingress-nginx `
+    --install `
     --namespace ingress-nginx `
     --set controller.replicaCount=3 `
+    --set controller.service.externalTrafficPolicy=Local `
     --set controller.nodeSelector."kubernetes\.io/os"=linux `
     --set defaultBackend.nodeSelector."kubernetes\.io/os"=linux `
     --set controller.admissionWebhooks.patch.nodeSelector."kubernetes\.io/os"=linux
     # private load balancer
-    # --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-resource-group"="$LOADBALANCER_RESOURCE_GROUP" `
+    # --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-resource-group"="$mcResourceGroupName" `
     # --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-internal"="true" `
     # --set controller.service.loadBalancerIP=$LoadBalancerIp
 
