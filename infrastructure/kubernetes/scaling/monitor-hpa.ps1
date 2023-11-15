b0591691edc98ce4d34f4767b2923fe63f10ddd0
Tobias Richling <richlint@apetito.com>
2023-11-15 17:03:36 +0100

    hpa / scaling

---
+++ infrastructure/kubernetes/scaling/monitor-hpa.ps1
@@ -0,0 +1,7 @@
+# watch the hpa / deployment
+kubectl get hpa microtodo-todos-api --watch
+kubectl get deployment microtodo-todos-api
+
+# generate load
+kubectl run -i --tty load-generator1 --rm --image=busybox:1.28 --restart=Never -- /bin/sh -c "while sleep 0.01; do wget -q -O- http://microtodo-todos-api:80/swagger/index.html; done"
+kubectl run -i --tty load-generator2 --rm --image=busybox:1.28 --restart=Never -- /bin/sh -c "while sleep 0.01; do wget -q -O- http://microtodo-todos-api:80/swagger/index.html; done"
