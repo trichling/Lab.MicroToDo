1940fd9872b711b215ea9a6f5a02f323731edf05
Tobias Richling <richlint@apetito.com>
2023-11-18 11:16:09 +0100

    remote debugger

--- application/frontend/src/Lab.MicroToDo.Frontend.Api/build.ps1
+++ application/frontend/src/Lab.MicroToDo.Frontend.Api/build.ps1
@@ -1,6 +1,11 @@
 $prevPwd = $PWD; Set-Location -ErrorAction Stop -LiteralPath $PSScriptRoot
 
-docker build -f Dockerfile -t thinkexception.azurecr.io/microtodo-frontendapi:dev .\..
+# dev build (with remote debugger)
+docker build --target publish -f Dockerfile -t thinkexception.azurecr.io/microtodo-frontendapi:dev --build-arg CONFIG=Debug .\..
+docker push thinkexception.azurecr.io/microtodo-frontendapi:dev
+
+# production build (withou remote debugger)
+docker build -f Dockerfile -t thinkexception.azurecr.io/microtodo-frontendapi:dev --build-arg CONFIG=Release .\..
 docker push thinkexception.azurecr.io/microtodo-frontendapi:dev
 
 $prevPwd | Set-Location
