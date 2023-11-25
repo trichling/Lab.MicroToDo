7dd25dacaf81ffff5d2a6124adf7e57f6ff91585
Tobias Richling <richlint@apetito.com>
2023-11-25 10:49:51 +0100

    make build scripts templatable

--- application/frontend/src/Lab.MicroToDo.Frontend/build.ps1
+++
@@ -1,6 +0,0 @@
-$prevPwd = $PWD; Set-Location -ErrorAction Stop -LiteralPath $PSScriptRoot
-
-docker build -f Dockerfile -t thinkexception.azurecr.io/microtodo-frontend:dev .\..
-docker push thinkexception.azurecr.io/microtodo-frontend:dev
-
-$prevPwd | Set-Location
