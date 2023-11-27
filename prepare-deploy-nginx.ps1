ccca8e7f35d03d318c20c0244faf34c774d2820e
Tobias Richling <richlint@apetito.com>
2023-11-27 18:34:39 +0100

    make ingress version aware

--- prepare-deploy-nginx.ps1
+++ prepare-deploy-nginx.ps1
@@ -1,15 +1,16 @@
 param (
-    [Parameter] [string] $Environment,
-    [Parameter] [string] $Version,
+    [Parameter()] [string] $Environment,
+    [Parameter()] [string] $Version
 )
 
-if ($Environment -eq $null) {
+if ($Environment -eq $null -or $Environment -eq "") {
     $Environment = "dev"
 }
 
-if ($Version -eq $null) {
+# if version is null or empty
+if ($Version -eq $null -or $Version -eq "") {
     $Version = "0"
 }
 
-prepare-branch.ps1 $Version 05-ssl
-deploy-feature-nginx.ps1
+./prepare-branch.ps1 $Version 05-ssl
+./deploy-feature-nginx.ps1
