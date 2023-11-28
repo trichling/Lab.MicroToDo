8eb7892d7c57cbe23dcf44276a4d422d1970cf3f
Tobias Richling <richlint@apetito.com>
2023-11-28 08:15:19 +0100

    fix / enhance scripts

--- infrastructure/azure/cluster-enablemonitoring.ps1
+++ infrastructure/azure/cluster-enablemonitoring.ps1
@@ -6,6 +6,7 @@ param (
 $location = "westeurope"
 $application = "microtodo"
 $resourceGroupName = "rg-$application-$Environment"
+$clusterName = "aks-$application-$Environment-$Version"
 $logAnalyticsWorkspaceName = "log-$application-$Environment"
 
 $logAnalyticsWorkspaceId = $(az monitor log-analytics workspace show --resource-group $resourceGroupName --workspace-name $logAnalyticsWorkspaceName --query id --output tsv)
