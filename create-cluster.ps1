c48980d770ec82179dd3f4d995e78164436f3a71
Tobias Richling <richlint@apetito.com>
2024-06-13 07:59:07 +0200

    fix on create cluster file

--- create-cluster.ps1
+++ create-cluster.ps1
@@ -1,7 +1,7 @@
 $Location = "germanywestcentral"
 
-.\infrastructure\azure\base.ps1 dev $Location
-.\infrastructure\azure\loganalyticsworkspace.ps1 dev
+.\infrastructure\azure\base.ps1 dev 10.1.4.0/22 $Location 
+.\infrastructure\azure\loganalyticsworkspace.ps1 dev 
 .\infrastructure\azure\cluster.ps1 dev 0 10.1.4.0
 .\infrastructure\azure\cluster-enableworkloadidentity.ps1 dev 0
 .\infrastructure\azure\workloadidentity.ps1 dev 0
@@ -10,7 +10,7 @@ $Location = "germanywestcentral"
 .\infrastructure\kubernetes\certmanager\install-certmanager.ps1 dev 0 nginx      
 # .\infrastructure\kubernetes\sqlserver\deploy-feature.ps1 
 
-.\prepare-branch.ps1 0 dev $Location
+.\prepare-branch.ps1 0 04-ingress $Location
 
 # .\application\todos\kubernetes\deploy-feature.ps1 
 .\application\frontend\kubernetes\api\deploy-feature.ps1
